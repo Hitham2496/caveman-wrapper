bf2e9d89b3d16b3fee62b0d3ba850066f366e9bf
Hitham Hassan <hh12@sanger.ac.uk>
2025-08-04 17:06:05 +0100

    Add some placeholder methods for caveman functionality to core.py

--- src/caveman_wrapper/core.py
+++ src/caveman_wrapper/core.py
@@ -27,3 +27,59 @@ class CavemanRunner():
         """
         if kwargs["help"]:
             print(self.HELP_MESSAGE)
+            return
+
+    def caveman_setup(self):
+        """
+        Runs CAVEMAN_SETUP from the parameters used at initialisation
+        """
+
+    def caveman_split(self):
+        """
+        Runs CAVEMAN_SPLIT from the parameters used at initialisation
+        """
+
+    def caveman_merge(self):
+        """
+        Runs CAVEMAN_MERGE from the parameters used at initialisation
+        """
+    
+    def caveman_mstep(self):
+        """
+        Runs CAVEMAN_MSTEP from the parameters used at initialisation
+        """
+    
+    def caveman_estep(self):
+        """
+        Runs CAVEMAN_ESTEP from the parameters used at initialisation
+        """
+
+    def caveman_merge_results(self):
+        """
+        Runs MERGE_CAVEMAN_RESULTS from the parameters used at initialisation
+        """
+
+    def caveman_add_vcf_ids(self):
+        """
+        Runs CAVEMAN_VCF_IDS from the parameters used at initialisation
+        """
+    
+    def caveman_split_vcf(self):
+        """
+        Runs CAVEMAN_VCF_SPLIT from the parameters used at initialisation
+        """
+
+    def count_files(self):
+        """
+        Runs FILE_COUNT from the parameters used at initialisation
+        """
+
+    def caveman_flag(self):
+        """
+        Runs CAVEMAN_FLAG from the parameters used at initialisation
+        """
+
+    def concat_flagged(self):
+        """
+        Runs CAVEMAN_VCF_FLAGGED_CONCAT from the parameters used at initialisation
+        """
