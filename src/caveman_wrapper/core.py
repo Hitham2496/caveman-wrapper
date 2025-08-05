c27a04fe91eaf40a06f015bb6c431b6be0467ea5
Hitham Hassan <hh12@sanger.ac.uk>
2025-08-05 11:59:42 +0100

    Add core functionality for helper parts of wrapper

--- src/caveman_wrapper/core.py
+++ src/caveman_wrapper/core.py
@@ -25,106 +25,148 @@ class CavemanRunner():
         """
         Initialises the runner from allowed key word arguments
         """
-        if kwargs["help"]:
-            print(self.HELP_MESSAGE)
-            return
 
-    def caveman_setup(self):
-        """
-        Runs CAVEMAN_SETUP from the parameters used at initialisation
-        """
+        # See if the user has asked for help before doing any more checking
+        # Exit after printing help message
+        if "help" in kwargs:
+            self.print_help_message()
+            sys.exit()
 
-    def caveman_split(self):
-        """
-        Runs CAVEMAN_SPLIT from the parameters used at initialisation
-        """
+        # Requesting manual should take precedence over checks too
+        # Exit after opening manual
+        if "man" in kwargs:
+            self.open_caveman_wrapper_manual()
+            sys.exit()
 
-    def caveman_merge(self):
+        # TODO: check if the caveman executable(s) are in the path
+
+        if "version" in kwargs:
+            self.print_caveman_version()
+            sys.exit()
+
+        allowed_keys = CavemanFlags.__annotations__
+
+        for key in kwargs:
+            if key not in allowed_keys:
+                raise ValueError(f"Key {key} is not recognised as an option for caveman")
+
+            if not isinstance(kwargs[key], allowed_keys[key]):
+                raise ValueError(f"The value provided for the parameter '{key}' should be of type: {allowed_keys[key]}")
+
+    def print_help_message(self):
         """
-        Runs CAVEMAN_MERGE from the parameters used at initialisation
+        Prints the help message for the wrapper usage
         """
+        print(self.HELP_MESSAGE)
     
-    def caveman_mstep(self):
+    def print_caveman_version(self):
         """
-        Runs CAVEMAN_MSTEP from the parameters used at initialisation
+        Prints the version of caveman if requested
         """
-    
-    def caveman_estep(self):
-        """
-        Runs CAVEMAN_ESTEP from the parameters used at initialisation
-        """
+        print(0.)
 
-    def caveman_merge_results(self):
+    def open_caveman_wrapper_manual(self):
         """
-        Runs MERGE_CAVEMAN_RESULTS from the parameters used at initialisation
+        Opens the caveman wrapper script manual page
         """
+        print("This will become the manual")
 
-    def caveman_add_vcf_ids(self):
-        """
-        Runs CAVEMAN_VCF_IDS from the parameters used at initialisation
-        """
-    
-    def caveman_split_vcf(self):
-        """
-        Runs CAVEMAN_VCF_SPLIT from the parameters used at initialisation
-        """
+   # def caveman_setup(self):
+   #     """
+   #     Runs CAVEMAN_SETUP from the parameters used at initialisation
+   #     """
 
-    def count_files(self):
-        """
-        Runs FILE_COUNT from the parameters used at initialisation
-        """
+   # def caveman_split(self):
+   #     """
+   #     Runs CAVEMAN_SPLIT from the parameters used at initialisation
+   #     """
 
-    def caveman_flag(self):
-        """
-        Runs CAVEMAN_FLAG from the parameters used at initialisation
-        """
+   # def caveman_merge(self):
+   #     """
+   #     Runs CAVEMAN_MERGE from the parameters used at initialisation
+   #     """
+   # 
+   # def caveman_mstep(self):
+   #     """
+   #     Runs CAVEMAN_MSTEP from the parameters used at initialisation
+   #     """
+   # 
+   # def caveman_estep(self):
+   #     """
+   #     Runs CAVEMAN_ESTEP from the parameters used at initialisation
+   #     """
 
-    def concat(self):
-        """
-        Runs concatenation for files from caveman running
-        """
+   # def caveman_merge_results(self):
+   #     """
+   #     Runs MERGE_CAVEMAN_RESULTS from the parameters used at initialisation
+   #     """
 
-    def concat_flagged(self):
-        """
-        Runs CAVEMAN_VCF_FLAGGED_CONCAT from the parameters used at initialisation
-        """
+   # def caveman_add_vcf_ids(self):
+   #     """
+   #     Runs CAVEMAN_VCF_IDS from the parameters used at initialisation
+   #     """
+   # 
+   # def caveman_split_vcf(self):
+   #     """
+   #     Runs CAVEMAN_VCF_SPLIT from the parameters used at initialisation
+   #     """
 
-    def zip_flagged(self):
-        """
-        Zips flagged files from caveman_flag stage
-        """
+   # def count_files(self):
+   #     """
+   #     Runs FILE_COUNT from the parameters used at initialisation
+   #     """
 
-    def pre_cleanup_zip(self):
-        """
-        Zip files before the cleanup stage if cleanup option is specified
-        """
+   # def caveman_flag(self):
+   #     """
+   #     Runs CAVEMAN_FLAG from the parameters used at initialisation
+   #     """
 
-    def limited_indices(self):
-        """
-        Checks whether the index is not greater than the limit or lower than 1
-        """
+   # def concat(self):
+   #     """
+   #     Runs concatenation for files from caveman running
+   #     """
 
-    def limited_flag_indices(self):
-        """
-        Check limited indices for the VCF split count
-        """
-        
-    def limited_xstep_indices(self):
-        """
-        Check limited indices for the split list count
-        """
+   # def concat_flagged(self):
+   #     """
+   #     Runs CAVEMAN_VCF_FLAGGED_CONCAT from the parameters used at initialisation
+   #     """
 
-    def load_exclude(self):
-        """
-        Loads the file of excludes and returns the exclude pattern
-        """
+   # def zip_flagged(self):
+   #     """
+   #     Zips flagged files from caveman_flag stage
+   #     """
 
-    def valid_seq_indices(self):
-        """
-        Checks that the sequence indices provided are valid
-        """
+   # def pre_cleanup_zip(self):
+   #     """
+   #     Zip files before the cleanup stage if cleanup option is specified
+   #     """
 
-    def extend_no_analysis(self):
-        """
-        Extends no analysis in caveman_merge results
-        """
+   # def limited_indices(self):
+   #     """
+   #     Checks whether the index is not greater than the limit or lower than 1
+   #     """
+
+   # def limited_flag_indices(self):
+   #     """
+   #     Check limited indices for the VCF split count
+   #     """
+   #     
+   # def limited_xstep_indices(self):
+   #     """
+   #     Check limited indices for the split list count
+   #     """
+
+   # def load_exclude(self):
+   #     """
+   #     Loads the file of excludes and returns the exclude pattern
+   #     """
+
+   # def valid_seq_indices(self):
+   #     """
+   #     Checks that the sequence indices provided are valid
+   #     """
+
+   # def extend_no_analysis(self):
+   #     """
+   #     Extends no analysis in caveman_merge results
+   #     """
