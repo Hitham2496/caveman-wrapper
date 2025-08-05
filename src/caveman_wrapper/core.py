92a97790967dea7cd9fbf03d52f9948b80c9cf9b
Hitham Hassan <hh12@sanger.ac.uk>
2025-08-05 16:36:53 +0100

    Add required steps for implementation to run_caveman, and identify caveman executable to a class var

--- src/caveman_wrapper/core.py
+++ src/caveman_wrapper/core.py
@@ -12,7 +12,7 @@ import sys
 import subprocess
 import time
 import multiprocessing
-from .utils import CavemanFlags
+from .utils import *
 
 class CavemanRunner():
     """
@@ -38,6 +38,7 @@ class CavemanRunner():
         if "man" in kwargs:
             self.open_caveman_wrapper_manual()
             sys.exit()
+
         # Raise an error if the caveman executabel is not in the path
         # Do this before checking version
         if not self.check_caveman_in_path():
@@ -51,10 +52,14 @@ class CavemanRunner():
 
         for key in kwargs:
             if key not in allowed_keys:
-                raise ValueError(f"Key {key} is not recognised as an option for caveman")
+                raise ValueError(f"Key '{key}' is not recognised as an option for caveman")
 
             if not isinstance(kwargs[key], allowed_keys[key]):
                 raise ValueError(f"The value provided for the parameter '{key}' should be of type: {allowed_keys[key]}")
+
+            setattr(self, key, kwargs[key])
+
+        self.run_caveman()
 
     def print_help_message(self):
         """
@@ -83,7 +88,45 @@ class CavemanRunner():
         `bool` - 
             True if found, False otherwise
         """
-        return bool(shutil.which("caveman"))
+        # TODO: Replace the 'True' with the commented out bit when done testing
+        self.caveman_executable = shutil.which("caveman")
+        return True # bool(self.caveman_executable)
+
+    def run_caveman(self):
+        """
+        Run caveman with the parameters identified at initialisation
+        """
+        # TODO: This is an outline of what needs to be added here
+        
+        # Step 1. set up processes in the same way as caveman.pl::setup
+
+        # Step 2. register processes i.e. if `threads` is given, use as many processes as requested
+
+        # Step 3. create temporary reference to be cleaned if reference is in `*.gz.fai` format
+
+        # Step 4. caveman_setup: if !process OR process == setup
+
+        # Step 5. caveman_split: if !process OR process == split
+
+        # Step 6. split_concat: if !process OR process == split_concat
+        
+        # Step 7. file_line_count: if !process OR process in [mstep, estep]
+
+        # Step 8. caveman_mstep: if !process OR process == mstep
+        
+        # Step 9. caveman_estep: if !process OR process == estep
+        
+        # Step 10. caveman_add_vcf_ids: if !process OR process == add_ids
+
+        # Step 11. caveman_flag: if !process OR process == flag OR !noflag
+
+        # Step 12. cleanup: if !noclean
+
+        # VALID_PROCESSES = ["setup", "split", "split_concat", "mstep", "merge", "estep", "merge_results", "add_ids", "flag"]
+        if self.process:
+            if not (self.process in CavemanConstants.VALID_PROCESSES):
+                raise ValueError(f"Process '{self.process}' is not a valid caveman process")
+        return
 
    # def caveman_setup(self):
    #     """
@@ -95,16 +138,16 @@ class CavemanRunner():
    #     Runs CAVEMAN_SPLIT from the parameters used at initialisation
    #     """
 
-   # def caveman_merge(self):
-   #     """
-   #     Runs CAVEMAN_MERGE from the parameters used at initialisation
-   #     """
-   # 
    # def caveman_mstep(self):
    #     """
    #     Runs CAVEMAN_MSTEP from the parameters used at initialisation
    #     """
-   # 
+
+   # def caveman_merge(self):
+   #     """
+   #     Runs CAVEMAN_MERGE from the parameters used at initialisation
+   #     """ 
+ 
    # def caveman_estep(self):
    #     """
    #     Runs CAVEMAN_ESTEP from the parameters used at initialisation
@@ -119,7 +162,12 @@ class CavemanRunner():
    #     """
    #     Runs CAVEMAN_VCF_IDS from the parameters used at initialisation
    #     """
-   # 
+
+   # def caveman_flag(self):
+   #     """
+   #     Runs CAVEMAN_FLAG from the parameters used at initialisation
+   #     """
+ 
    # def caveman_split_vcf(self):
    #     """
    #     Runs CAVEMAN_VCF_SPLIT from the parameters used at initialisation
@@ -128,11 +176,6 @@ class CavemanRunner():
    # def count_files(self):
    #     """
    #     Runs FILE_COUNT from the parameters used at initialisation
-   #     """
-
-   # def caveman_flag(self):
-   #     """
-   #     Runs CAVEMAN_FLAG from the parameters used at initialisation
    #     """
 
    # def concat(self):
@@ -164,7 +207,7 @@ class CavemanRunner():
    #     """
    #     Check limited indices for the VCF split count
    #     """
-   #     
+     
    # def limited_xstep_indices(self):
    #     """
    #     Check limited indices for the split list count
